f0dbb354f8cbc96cd02597648347b847e8194c33
Anton Parfonov <antonparfonov@gmail.com>
2024-03-20 17:38:56 +0200

    Minor changes

--- tests/index.rs
+++ tests/index.rs
@@ -9,11 +9,7 @@ async fn index_works() {
     let app_url = format!("http://{}", app.config.connection.to_string());
 
     let client = reqwest::Client::new();
-    let response = client
-        .get(format!("{app_url}/"))
-        .send()
-        .await
-        .unwrap();
+    let response = client.get(format!("{app_url}/")).send().await.unwrap();
 
     assert!(response.status().is_success());
     assert_ne!(response.content_length(), Some(0));
